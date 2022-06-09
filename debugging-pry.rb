4a8d32c774e50bc304cb4b5218f5fee17eddb2fb
MissDine <enidnyatichi1212@gmail.com>
2022-06-09 13:48:01 +0300

    Feature edit

---
+++ debugging-pry.rb
@@ -0,0 +1,10 @@
+# reverse_each_word that takes in a argument of a sentence as a string, and returns that same sentence with each word reversed in place.
+greet = ("Hello,Enid.")
+greetings = "How have you been?"
+
+puts greet.reverse
+puts greet.split
+
+puts Array.new << "Hello gal"
+puts greet.join("How have you been")
+
